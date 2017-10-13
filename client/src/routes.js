bda57cbf533aff538146f860444a5264768619ea
ChintanVachhani <chintan.vachhani10@gmail.com>
2017-10-13 02:55:58 -0700

    Implemented client side 'signin' & 'signup' with 'User' component.

--- client/src/routes.js
+++ client/src/routes.js
@@ -3,10 +3,12 @@ import {Route, IndexRoute} from 'react-router';
 
 import App from './components/App';
 import User from './components/User';
+import Header from './components/Header';
 
 export default (
   <Route path="/" component={App}>
-    <IndexRoute component={User}/>
+    <IndexRoute component={Header}/>
     <Route path="login" component={User}/>
+    <Route path="header" component={Header}/>
   </Route>
 );
