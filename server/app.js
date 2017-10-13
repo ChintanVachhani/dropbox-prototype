b8086526588a2da2bbd3769e7977e70a255e394f
ChintanVachhani <chintan.vachhani10@gmail.com>
2017-10-12 18:58:50 -0700

    Restructured Project Files and basic React Client setup.

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let bodyParser = require('body-parser');
+let cors = require('cors');
 
 // Initialize the Express App
 let app = express();
@@ -17,11 +18,16 @@ let activityRoutes = require('./routes/activity');
 let groupRoutes = require('./routes/group');
 let groupFileRoutes = require('./routes/groupFile');
 
-// uncomment after placing your favicon in /public
+// CORS settings
+let corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(cors(corsOptions));
+
 app.use(bodyParser.json({limit: '20mb'}));
 app.use(bodyParser.urlencoded({limit: '20mb', extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join('public')));
 
 // API paths
 app.use('/user', userRoutes);
@@ -33,19 +39,18 @@ app.use('/activity', activityRoutes);
 app.use('/group', groupRoutes);
 app.use('/groupFile', groupFileRoutes);
 
-app.use('/', function (req, res, next) {
-    return res.sendFile(path.join('public', 'index.html'));
-});
-
 // catch 404 and render index page
 app.use(function (req, res, next) {
-    return res.sendFile(path.join('public', 'index.html'));
+  res.status(404).json({
+    title: 'Bad API call.',
+    error: {message: 'Page not found.'},
+  });
 });
 
 // start app
 app.listen(serverConfig.port, (error) => {
     if (!error) {
-        console.log(`dropbox-prototype is running on port: ${serverConfig.port}!`); // eslint-disable-line
+        console.log(`dropbox-prototype is running on port: ${serverConfig.port}!`);
     }
 });
 
